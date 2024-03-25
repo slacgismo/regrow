751a335253961ab1896e5ac962a5a760c9d0d522
Alyona Teyber <Ivanova.alyona5@gmail.com>
2024-03-25 12:18:08 -0700

    Update eia.py

--- data/EIA/eia.py
+++ data/EIA/eia.py
@@ -9,7 +9,7 @@ def main():
     """
     #Create EIA API using your specific API key
     api_key = ""
-    api_url = "https://api.eia.gov/v2/electricity/retail-sales/data/?frequency=annual&data[0]=sales&facets[sectorid][]=COM&facets[sectorid][]=IND&facets[sectorid][]=OTH&facets[sectorid][]=RES&facets[sectorid][]=TRA&start=2019&end=2023&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000"
+    api_url = "https://api.eia.gov/v2/electricity/retail-sales/data/?frequency=annual&data[0]=sales&facets[sectorid][]=COM&facets[sectorid][]=IND&facets[sectorid][]=OTH&facets[sectorid][]=RES&facets[sectorid][]=TRA&start=2018&end=2023&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000"
     response = requests.get(f"{api_url}&api_key={api_key}")
     # print(response.json())
     df = pd.DataFrame(response.json()["response"]["data"])

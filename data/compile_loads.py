3d4abfd2a48ba7a431f4b2bf3c22ab8c20f877c2
David P. Chassin <dchassin@slac.stanford.edu>
2024-04-03 14:54:19 -0700

    Connected buildings to busses

--- data/compile_loads.py
+++ data/compile_loads.py
@@ -1,63 +1,47 @@
 import marimo
 
-__generated_with = "0.1.82"
+__generated_with = "0.3.4"
 app = marimo.App(width="full")
+
+
+@app.cell
+def __(ns, pd):
+    #
+    # Load bus gis data
+    #
+    bus = pd.read_csv("wecc240_gis.csv")
+    bus["geocode"] = [ns.geohash(x,y,6) for x,y in zip(bus["Lat"],bus["Long"])]
+    bus.set_index("geocode",inplace=True)
+    bus.sort_index(inplace=True)
+
+    return bus,
 
 
 @app.cell
 def __(pd):
     #
-    # Load bus gis data
+    # Load building data
     #
-    gis = pd.read_csv("wecc240_gis.csv")
-    return gis,
+    buildings = pd.read_csv("buildings.csv").set_index(["geocode","BuildingType"]).sort_index()
+    return buildings,
 
 
 @app.cell
-def __(os, pd):
-    #
-    # Process buildings in county files
-    #
-    if os.path.exists("building_data.csv"):
-        data = pd.read_csv("building_data.csv",index_cols=["County","BuildingType"])
-    else:
-        data = []
-        for _file in [x for x in os.listdir("buildings") if x.endswith(".csv.zip")]:
-            _county = _file.split(".")[0]
-            _data = pd.read_csv(
-                os.path.join("buildings", _file),
-                usecols=["FloorArea", "BuildingType", "latitude", "longitude"],
-            )
-            _building_type = _data.groupby("BuildingType")
-            _result = pd.DataFrame(_building_type.sum()["FloorArea"]/1e6).round(2)
-            _result["BuildingCount"] = _building_type.count()["FloorArea"]
-            _result["County"] = _county
-            data.append(_result.reset_index())
-        data = pd.concat(data)
-        data.set_index(["County","BuildingType"],inplace=True)
-        data.to_csv("building_data.csv",index=True,header=True)
-    data
-    return data,
-
-
-@app.cell(disabled=True, hide_code=True)
-def __(array, gen, gis):
-    #
-    # Match buildings to busses based on gis data
-    #
-    _xx,_yy = array(gis.Lat),array(gis.Long)
-    names = gis[gis.columns[0]]
-    with open("powerplants_gis.glm","w") as _fh:
-        for _name,_plant in gen.items():
-            if not "latitude" in _plant:
-                print(f"// powerplant {_name} does not have a location",file=_fh)
+def __(array, buildings, bus):
+    _gis = bus.reset_index()
+    _xx,_yy = array(_gis.Lat),array(_gis.Long)
+    _names = _gis[_gis.columns[0]]
+    with open("buildings_gis.glm","w") as _fh:
+        for _name,_data in buildings.iterrows():
+            if not "latitude" in _data:
+                print(f"// building {'_'.join(_name)} does not have a location",file=_fh)
                 continue
-            _x,_y = float(_plant["latitude"]),float(_plant["longitude"])
+            _x,_y = float(_data["latitude"]),float(_data["longitude"])
             _dx,_dy = _xx-_x,_yy-_y
-            _d = zip(_dx*_dx + _dy*_dy,names)
+            _d = zip(_dx*_dx + _dy*_dy,_names)
             _dn = sorted(_d,key=lambda x:x[0])
-            print("modify",f"{_name}.parent",f"wecc240_psse_N_{_dn[0][1]};",file=_fh)
-    return names,
+            print("modify",f"{'_'.join(_name)}.parent",f"wecc240_psse_N_{_dn[0][1]};",file=_fh)
+    return
 
 
 @app.cell
@@ -65,8 +49,10 @@ def __():
     import os, sys
     import marimo as mo
     import pandas as pd
-    pd.options.display.max_rows=100
-    return mo, os, pd, sys
+    from numpy import array
+    sys.path.append("/usr/local/opt/gridlabd/current/share/gridlabd")
+    import nsrdb_weather as ns
+    return array, mo, ns, os, pd, sys
 
 
 if __name__ == "__main__":

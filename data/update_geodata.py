5076c22739fd3b185fa5255e7e09c13a42d8bb33
dchassin <dchassin@slac.stanford.edu>
2024-04-18 19:45:16 -0700

    Fill missing geodata

--- data/update_geodata.py
+++ data/update_geodata.py
@@ -4,6 +4,7 @@ import os, sys
 import pandas as pd
 
 outputs = {
+	"Unnamed: 0" : None,
 	"ghi" : "solar",
 	"temp_air" : "temperature",
 	}
@@ -14,16 +15,24 @@ for n,file in enumerate(files):
 		continue
 	geocode = file.replace(".csv.zip","")
 	print("Processing",file,end="...",flush=True)
-	data = pd.read_csv(os.path.join("nsrdb",file),index_col=[0],parse_dates=[0],date_format="ISO8601")
+	data = pd.read_csv(os.path.join("nsrdb",file),
+		index_col=[0],
+		usecols=outputs.keys(),
+		parse_dates=[0],
+		date_format="ISO8601")
 	for source,output in outputs.items():
+		if not output:
+			continue
 		if output not in results:
 			results[output] = []
 		try:
-			results[output].append(pd.DataFrame({geocode:data[source].resample("1h").mean()}))
+			results[output].append(pd.DataFrame({geocode:data[source].resample("1h").mean()}).round(1).fillna(method='ffill'))
 		except Exception as err:
 			print(source,"failed","-",err,end="!!!")
 	print(f"{n+1} of {len(files)} done")
 
 for output in outputs.values():
+	if not output:
+		continue
 	results[output] = pd.concat(results[output],axis=1)
-	results[output].to_csv(os.path.join("geodata",output+".csv.zip"),index=True,header=True,compression="zip")
+	results[output].to_csv(os.path.join("geodata",output+".csv"),index=True,header=True)

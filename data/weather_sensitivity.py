f0a8ff9fa4a948f32f3c826e884377da592ece4b
David P. Chassin <dchassin@slac.stanford.edu>
2024-04-26 11:29:05 -0700

    Update weather_sensitivity.py

--- data/weather_sensitivity.py
+++ data/weather_sensitivity.py
@@ -1,46 +1,137 @@
+"""Update weather sensitivity
+
+Syntax: python3 -m weather_sensitivity OPTIONS ...
+
+Options:
+    --cooling=DEGC  set the cooling cutoff temperature (default is 10C)
+    --freq=FREQ     set the data resample frequency (default is 1H)
+    -h|--help|help  generate this help document
+    --heating=DEGC  set the heating cutoff temperature (default is 25C)
+    --inputs        generate list of input files
+    --outputs       generate list of output files
+    --update        update only missing/outdates files
+    --verbose       verbose progress updates
+
+The sensivity analysis generates the weather sensitivity for all loads in each
+weather location found in the load files.  The nearest weather location is
+used to provide solar and temperature data.
+"""
+
 import os, sys
+import numpy as np
 import pandas as pd
-pd.options.display.max_columns = None
-pd.options.display.width = 1024
+import datetime as dt
+import geocode as gc
 
-Theat = 60
-Tcool = 80
-server = "https://oedi-data-lake.s3.amazonaws.com/nrel-pds-building-stock/end-use-load-profiles-for-us-building-stock/2021/resstock_amy2018_release_1/weather/amy2018/"
-pumas = {
-    "Alameda" : "G0600010",
-    "Contra Costa" : "G0600130",
-    "Los Angeles" : "G0600370",
-    "Riverside" : "G0600650",
-    "San Bernadino" : "G0600710",
-    "San Diego" : "G0600730",
-    "San Francisco" : "G0600750",
-    "San Mateo" : "G0600810",
-    "Santa Clara" : "G0600850",
+INPUTS = {
+    "TEMPERATURE" : "geodata/temperature.csv",
+    "SOLAR" : "geodata/solar.csv",
+    "COMMERCIAL" : "geodata/commercial.csv",
+    "RESIDENTIAL" : "geodata/residential.csv",
+    "INDUSTRIAL" : "geodata/industrial.csv",
+    "TRANSPORTATION" : "geodata/transportation.csv",
+    "AGRICULTURAL" : "geodata/agricultural.csv",
+    }
+
+OUTPUTS = {
+    "BASELOAD" : "geodata/baseload.csv",
+    "SENSITIVITY" : "weather_sensitivity.csv",
 }
+FORCE = False
+VERBOSE = False
+TCOOL = 25 # degC cooling cutoff temperature
+THEAT = 10 # degC heating cutoff temperature
+FREQ = "1h"
 
-print("Loading loadshapes...",flush=True)
-loadshapes = pd.read_csv("loadshapes.csv.zip",
-    index_col = ["county","building_type","datetime"],
-    )
+if len(sys.argv) == 1:
+    print(__doc__)
+    exit(1)
+elif "--inputs" in sys.argv:
+    print(" ".join(INPUTS.values()))
+    exit(0)
+elif "--outputs" in sys.argv:
+    print(" ".join(OUTPUTS.values()))
+    exit(0)
 
-for county,puma in pumas.items():
-    file = f"weather/CA-{county.replace(' ','_')}.csv.zip"
-    if not os.path.exists(file):
-        print("Downloading",file,"...",flush=True)
-        data = pd.read_csv(os.path.join(server,f"{puma}_2018.csv"),
-            index_col = ["date_time"],
-            )
-        data.index.name = "datetime"
-        data.columns = ["TD[degC]","RH[%]","WS[m/s]","WD[deg]","GH[W/m^2]","DN[W/m^2]","DH[W/m^2]"]
-        data.round(1).to_csv(file,index=True,header=True,compression="zip")
-    else:
-        print("Loading weather for",file,"...",flush=True)
-    data = pd.read_csv(file,index_col="datetime")
+for arg in sys.argv[1:]:
+    if arg.startswith("--heating="):
+        THEAT = float(arg.split("=")[1])
+    elif arg.startswith("--cooling="):
+        TCOOL = float(arg.split("=")[1])
+    elif arg.startswith("--freq"):
+        FREQ = arg.split("=")[1]
+    elif "--verbose" in sys.argv:
+        VERBOSE = True
+    elif arg != "--update":
+        raise Exception(f"option '{arg}' is not valid")
 
-    heating = (data["TD[degC]"]<Theat).index
-    cooling = (data["TD[degC]"]>Tcool).index
-    solar = (data["DN[W/m^2]"]>0).index
+def verbose(msg,**kwargs):
+    if VERBOSE:
+        print(msg,file=sys.stderr,flush=True,**kwargs)
 
-    loads = loadshapes.loc[county]
-    for building_type in loads.index.get_level_values(0).unique():
-        print("Processing",county,"County",building_type,"...",flush=True)
+if __name__ == "__main__" and "--update" in sys.argv:
+
+    verbose("Loading input files",end="...")
+    temperature = pd.DataFrame(pd.read_csv(INPUTS["TEMPERATURE"],index_col=[0],parse_dates=[0]).resample(FREQ).mean())
+    solar = pd.DataFrame(pd.read_csv(INPUTS["SOLAR"],index_col=[0],parse_dates=[0]).resample(FREQ).mean())
+    load = pd.DataFrame(pd.read_csv(INPUTS["COMMERCIAL"],index_col=[0],parse_dates=[0]).resample(FREQ).mean())
+    # TODO: add other inputs
+    verbose("done")
+
+    columns = [f"WD{n:02d}" for n in range(24)]+[f"WE{n:02d}" for n in range(24)]+["TH","TC","S"]
+    def get_model(geohash):
+        _T = pd.DataFrame({"T":temperature[gc.nearest(geohash,temperature.columns)]})
+        _S = pd.DataFrame({"S":solar[gc.nearest(geohash,solar.columns)]})
+        _L = pd.DataFrame({"L":load[geohash]})
+        _L.index = pd.to_datetime(_L.index-dt.timedelta(hours=7),utc=True)
+        data = _T.join(_S).join(_L).dropna()
+        T = data["T"].tolist()
+        S = data["S"].tolist()
+        L = data["L"].tolist()
+        t = data.index
+        
+        M = [[1 if x.weekday()<5 else 0 for x in t]]
+        M.extend([[1 if x.weekday()<5 and x.hour == h else 0 for x in t] for h in range(1,24)])
+        M.append([1 if x.weekday()>4 else 0 for x in t])
+        M.extend([[1 if x.weekday()>4 and x.hour == h else 0 for x in t] for h in range(1,24)])
+        M.extend([
+            [THEAT-x if x<THEAT else 0 for x in T], # heating load
+            [TCOOL-x if x>TCOOL else 0 for x in T], # cooling load
+            S, # solar load]
+        ])
+        M= np.array(M).T
+        x = np.linalg.solve(M.T@M,M.T@L)
+        Y = M@x
+        ls = {"Weekday":[x[0]] + [x[0]+y for y in x[1:24]],
+              "Weekend":[x[24]] + [x[24]+y for y in x[25:48]],
+             }
+        return x,M,Y,ls,t,L,T,S
+
+    result = []
+    loads = []
+    for geohash in load.columns:
+        verbose(f"Processing {geohash}",end="...")
+        try:
+            x,M,Y,ls,t,L,T,S = get_model(geohash)
+            a,b,c = x[-3:]
+            Lb = np.array(L) - a*np.array(S) - b*np.array([THEAT-x if x<THEAT else 0 for x in T]) - c*np.array([TCOOL-x if x>TCOOL else 0 for x in T])
+            verbose("done")
+        except Exception as err:
+            pd.DataFrame(M,columns=columns).round(4).to_csv(f"weather_sensitivity_{str(err).lower().replace(' ','-')}_{geohash}-M_err.csv")
+            Lb = L
+            x=[0,0,0]*3
+            verbose(err)
+        result.append(pd.DataFrame(data=[x[-3:]],index=[geohash],
+                                   columns=["heating[MW/degC]","cooling[MW/degC","solar[MW/W/m^2]"]))
+        loads.append(pd.DataFrame(data=Lb,index=t,columns=[geohash]).round(4))
+
+    verbose("Saving output files",end="...")
+    result = pd.concat(result).round(3)
+    result.index.name = "geocode"
+    result.to_csv(OUTPUTS["SENSITIVITY"],index=True,header=True)
+
+    loads = pd.concat(loads,axis=1)
+    loads.index.name = "timestamp"
+    loads.to_csv(OUTPUTS["BASELOAD"])
+    verbose("done")
+

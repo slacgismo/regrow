b564e15093d57c6387c77248f8fa869f02e3a60c
David P. Chassin <dchassin@slac.stanford.edu>
2024-05-13 16:58:54 -0700

    Update nuclear plant data

--- data/powerplants/nuclear.py
+++ data/powerplants/nuclear.py
@@ -14,15 +14,51 @@ OUTPUTS = {
 STARTYEAR=2018
 STOPYEAR=2022
 
-data = pd.read_excel(INPUTS["GLOBALDATA"],sheet_name="Data",
-	usecols=[1,2,3,6,7,8,9,10,11,12,23,24,25,26,27,]
-	)
+data = pd.read_excel(INPUTS["GLOBALDATA"],sheet_name="Data")
 
-data.columns = ["country","name","unit_id","capacity[MW]","status",
-	"reactor_type","reactor_model","start[y]","retirement[y]","planned_retirement[y]",
-	"reference_capacity[MW]","design_capacity[MW]","thermal_capacity[MW]",
-	"latitude","longitude",
-]
+mapper = {
+	'Date Last Researched' : None, 
+	'Country' : "country", 
+	'Project Name' : "name", 
+	'Unit Name' : "unit_id",
+	'Project Name in Local Language / Script' : None, 
+	'Other Name(s)' : None,
+	'Capacity (MW)' : "capacity[MW]", 
+	'Status' : "status", 
+	'Reactor Type' : "type", 
+	'Model' : "model", 
+	'Start Year' : "start[y]",
+	'Retirement Year' : "retirement[y]", 
+	'Planned Retirement' : None, 
+	'Cancellation Year' : None,
+	'Construction Start Date' : None, 
+	'First Criticality Date' : None,
+	'First Grid Connection' : None, 
+	'Commercial Operation Date' : None, 
+	'Retirement Date' : None,
+	'Owner' : None, 
+	'Owner Name in Local Language / Script' : None, 
+	'Operator' : None,
+	'Operator Name in Local Language / Script' : None,
+	'Reference Net Capacity (MW)' : "reference_capacity[MW]", 
+	'Design Net Capacity (MW)' : "design_capacity[MW]",
+	'Thermal Capacity (MWt)' : "thermal_capacity[MW]", 
+	'Latitude' : "latitude", 
+	'Longitude' : "longitude", 
+	'Location Accuracy' : None,
+	'City' : None, 
+	'Local Area (taluk, county)' : "county",
+	'Major Area (prefecture, district)' : None, 
+	'State/Province' : "state", 
+	'Subregion' : None,
+	'Region' : None, 
+	'GEM location ID' : None, 
+	'GEM unit ID' : None, 
+	'Wiki URL' : None,
+	}
+
+data.rename(dict([(x,y) for x,y in mapper.items() if not y is None]),inplace=True,axis=1)
+data.drop([x for x,y in mapper.items() if y is None],axis=1,inplace=True)
 data.drop(data[~data["status"].isin(["operating","retired"])].index,inplace=True)
 data.drop(data[data["country"]!="United States"].index,inplace=True)
 data.drop(data[data["retirement[y]"]<STARTYEAR].index,inplace=True)

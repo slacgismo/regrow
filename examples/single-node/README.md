7bdfd64a80fbe5c669e8fc26274dd909f84f9629
David P. Chassin <david.chassin@me.com>
2024-07-21 09:08:50 -0700

    Add single node WIP

---
+++ examples/single-node/README.md
@@ -0,0 +1,21 @@
+# Setup
+
+## First time
+
+~~~
+python3 -m venv .
+source bin/activate
+python3 -m pip install pip --upgrade -r requirements.txt
+~~~
+
+## Returning to work
+
+~~~
+source bin/activate
+~~~
+
+# Running
+
+~~~
+marimo run single_node.py
+~~~
---
+++ examples/single-node/single_node.py
@@ -0,0 +1,225 @@
+import marimo
+
+__generated_with = "0.7.9"
+app = marimo.App(width="full")
+
+
+@app.cell
+def __():
+    #
+    # Simulation environment and defaults
+    #
+    DATA_SOURCE = "../../data/geodata"
+    DATA_FILES = ["temperature","solar","wind","baseload","heating","cooling","total"]
+    DEFAULT_LOCATION = "9q9wtp"
+    return DATA_FILES, DATA_SOURCE, DEFAULT_LOCATION
+
+
+@app.cell
+def __():
+    #
+    # Wind performance
+    #
+    wind_cutin = 5  # m/s
+    wind_allin = 10  # m/s
+    wind_cutout = 25  # m/s
+    wind_rating = 100.0  # MW
+
+    def wind_power(x):
+        x[x<=wind_cutin] = 0
+        x[x>=wind_cutout] = 0
+        x[(x>wind_cutin)&(x<=wind_allin)] = (wind_allin-x)/(wind_allin-wind_cutin)*wind_rating
+        x[(x>wind_allin)&(x<wind_cutout)] = wind_rating
+        return x
+    return wind_allin, wind_cutin, wind_cutout, wind_power, wind_rating
+
+
+@app.cell
+def __():
+    #
+    # Solar performance
+    #
+    solar_efficiency = 0.10  # pu
+    solar_panelarea = 1000000.0  # m^2
+
+    def solar_power(x):
+        return x * solar_efficiency * solar_panelarea / 1e6
+    return solar_efficiency, solar_panelarea, solar_power
+
+
+@app.cell
+def __(data, get_location, pd):
+    #
+    # Generation dispatch
+    #
+    L = sum([data[x][get_location()] for x in ["baseload","cooling","heating"]])  # loads 
+    R = sum([data[x][get_location()] for x in ["solar","wind"]])  # renewables 
+    G = pd.DataFrame(data=L - R, index=L.index)
+    return G, L, R
+
+
+@app.cell
+def __(DATA_SOURCE, os, pd, solar_power, wind_power):
+    #
+    # Datasets
+    #
+    datasets = {"temperature": {"converter": lambda x: x*1.8+32, "unit": "$^o$F"},
+                "solar": {"converter": solar_power, "unit": "MW"}, 
+                "wind": {"converter": wind_power, "unit": "MW"},
+                "cooling": {"unit": "MW"},
+                "heating": {"unit": "MW"},
+                "baseload": {"unit": "MW"},
+                "total": {"unit": "MW"},
+               }
+    data = {}
+    for name,spec in datasets.items():
+        file = os.path.join(DATA_SOURCE,name+".csv")
+        opts = dict(low_memory=True,header=[0],index_col=0,parse_dates=[0])
+        data[name] = spec["converter"](pd.read_csv(file,**opts)) if "converter" in spec else pd.read_csv(file,**opts)
+    start = min([data[x].index[0] for x in datasets])
+    stop = max([data[x].index[-1] for x in datasets])
+    return data, datasets, file, name, opts, spec, start, stop
+
+
+@app.cell
+def __(DEFAULT_LOCATION, mo):
+    #
+    # Location state variable
+    #
+    get_location, set_location = mo.state(DEFAULT_LOCATION)
+    get_day, set_day = mo.state(0)
+    get_range, set_range = mo.state(365)
+    return (
+        get_day,
+        get_location,
+        get_range,
+        set_day,
+        set_location,
+        set_range,
+    )
+
+
+@app.cell
+def __(data, datasets, get_location, mo, set_location):
+    #
+    # Choose location
+    #
+    locations = sorted(list(
+        set.intersection(*[set(data[x].columns[1:]) for x in datasets])
+    ))
+    location_ui = mo.ui.dropdown(
+        label="Location:",
+        value=get_location(),
+        on_change=set_location,
+        options=locations,
+        allow_select_none=False,
+    )
+    return location_ui, locations
+
+
+@app.cell
+def __(get_range, mo, set_range):
+    #
+    # Time range slider
+    #
+    range_ui = mo.ui.slider(
+        steps=[1, 7, 31, 92, 365],
+        value=get_range(),
+        on_change=set_range,
+        debounce=True,
+    )
+    return range_ui,
+
+
+@app.cell
+def __(get_day, get_range, mo, range_ui, set_day, start, stop):
+    #
+    # Day slider
+    #
+    day_ui = mo.ui.slider(
+        steps=list(range(0,int(stop.as_unit("s").value/3600e9/24) - int(start.as_unit("s").value/3600e9/24),range_ui.value)),
+        value=get_day() % get_range(),
+        on_change=set_day,
+        debounce=True,
+    )
+    return day_ui,
+
+
+@app.cell
+def __(get_day, mo, range_ui, set_day):
+    #
+    # Time range navigation button
+    #
+    prev_ui = mo.ui.button(label="Back",on_click=lambda x:set_day(get_day()-range_ui.value))#max(get_day()-range_ui.value,int(start.as_unit("s").value/3600e9/24))))
+    next_ui = mo.ui.button(label="Next",on_click=lambda x:set_day(get_day()+range_ui.value))#min(get_day()+range_ui.value),int(stop.as_unit("s").value/3600e9/24-range_ui.value)))
+    return next_ui, prev_ui
+
+
+@app.cell
+def __(
+    day_ui,
+    get_day,
+    get_range,
+    location_ui,
+    mo,
+    next_ui,
+    pd,
+    prev_ui,
+    range_ui,
+    start,
+):
+    #
+    # Main display
+    #
+    mo.hstack(
+        [
+            location_ui,
+            mo.md(f"Window by {get_range()}"),
+            range_ui,
+            mo.md(f"day(s), starting on {start+pd.Timedelta(days=get_day())} (day"),
+            day_ui,
+            mo.md(")"),
+            prev_ui,
+            next_ui,
+        ],
+        justify="start",
+    )
+    return
+
+
+@app.cell
+def __(G, data, day_ui, get_location, mo, plt, range_ui):
+    #
+    # Tabs
+    #
+    _tabs = {}
+    _opts = dict(grid=True, figsize=(30, 10), xlabel="Date/Time")
+    for _x, _y in data.items():
+        plt.figure()
+        _units = "$^o$C" if _x in ["temperature"] else "MW"
+        _tabs[_x.title()] = _y[int(day_ui.value*24) : int(day_ui.value + range_ui.value)*24][get_location()].plot(
+            ylabel=f"{_x.title()} [{_units}]", **_opts
+        )
+    plt.figure()
+    _tabs["Dispatch"] = G[int(day_ui.value*24) : int(day_ui.value + range_ui.value) * 24][G > 0].plot(ylabel=f"Dispatch [MW]", label="OK", **_opts)
+    plt.plot(G[int(day_ui.value*24) : int(day_ui.value + range_ui.value) * 24][G <= 0], "xr", label="Outage")
+    mo.ui.tabs(_tabs, lazy=True)
+    return
+
+
+@app.cell
+def __():
+    #
+    # Modules
+    #
+    import os, sys
+    import marimo as mo
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from collections.abc import Iterable
+    return Iterable, mo, np, os, pd, plt, sys
+
+
+if __name__ == "__main__":
+    app.run()

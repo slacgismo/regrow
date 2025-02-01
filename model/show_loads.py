71b848d56b00dfd5925e0492056e184c665ac14d
David P. Chassin <david.chassin@me.com>
2025-02-01 10:55:43 -0800

    WIP bus selection

--- model/show_loads.py
+++ model/show_loads.py
@@ -5,20 +5,68 @@ app = marimo.App(width="medium")
 
 
 @app.cell
-def _(geojson, mo, pd):
+def _(mo):
+    mo.md(r"""# WECC 240 Loads""")
+    return
+
+
+@app.cell
+def _(mo):
+    # Control time and place
+
+    get_hour,set_hour = mo.state(0)
+    get_location,set_location = mo.state(None)
+    return get_hour, get_location, set_hour, set_location
+
+
+@app.cell
+def _(geojson, pd):
     # Load data
+
     loads = pd.read_csv("loads.csv",
         parse_dates=[0],
         index_col="datetime",
         )
     usmap = geojson.load(open("../data/geojson/usmap.geojson","r"))
-    get_hour,set_hour = mo.state(0)
-    return get_hour, loads, set_hour, usmap
+    return loads, usmap
+
+
+@app.cell
+def _(loads, np, timestamp_ui, timestamps):
+    # Get geocode points
+
+    geocodes = loads.loc[timestamps[timestamp_ui.value]].set_index("geocode")
+    points = np.stack([geocodes.longitude.tolist(), geocodes.latitude.tolist()], -1)
+    return geocodes, points
+
+
+@app.cell(hide_code=True)
+def _(loads, np, plt, spatial):
+    # WIP: map load
+
+    _loads = loads.loc[loads.index[0]]
+    _points = np.array([_loads.longitude.tolist(), _loads.latitude.tolist()]).transpose()
+    _map = spatial.Voronoi(_points)
+    # spatial.voronoi_plot_2d(_map)
+    plt.show()
+
+    def area(x,y):
+        return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
+    _regions = list(zip(_map.points.tolist(),[[tuple(_map.vertices[y]) for y in _map.regions[x]] for x in _map.point_region]))
+    _geocodes = _loads.set_index("geocode")
+    _geocodes = {x:np.unique(_geocodes.loc[x][["longitude","latitude"]].values).tolist() for x in _geocodes.index.unique()}
+    # print(_geocodes)
+    # print(_regions)
+    # for _point,_location in _geocodes.items():
+
+    # for _location,_region in regions:
+    return (area,)
 
 
 @app.cell
 def _(loads, mo, set_hour):
     # Setup UI slider
+
     timestamps = loads.index.unique().tolist()
     timestamp_ui = mo.ui.slider(
         start=0,
@@ -32,19 +80,33 @@ def _(loads, mo, set_hour):
 @app.cell
 def _(get_hour, loads, mo, set_hour):
     # Setup UI buttons
+
     start_ui = mo.ui.button(label="$|\lt$",on_click=lambda x:set_hour(0))
     subday_ui = mo.ui.button(label="$\lt\lt$",on_click=lambda x:set_hour(max(get_hour()-24,0)))
     subhour_ui = mo.ui.button(label="$\lt$",on_click=lambda x:set_hour(max(get_hour()-1,0)))
     addhour_ui = mo.ui.button(label="$\gt$",on_click=lambda x:set_hour(min(get_hour()+1,len(loads.index)-1)))
     addday_ui = mo.ui.button(label="$\gt\gt$",on_click=lambda x:set_hour(min(get_hour()+24,len(loads.index)-1)))
     end_ui = mo.ui.button(label="$\gt|$",on_click=lambda x:set_hour(len(loads.index)-1))
-    return addday_ui, addhour_ui, end_ui, start_ui, subday_ui, subhour_ui
+    _options = {"Voltage magnitude":"voltage[pu]","Voltage angle":"voltage[deg]"}
+    voltage_ui = mo.ui.dropdown(options=_options,value=list(_options)[0])
+    browser_ui = mo.ui.checkbox(label="Show data")
+    return (
+        addday_ui,
+        addhour_ui,
+        browser_ui,
+        end_ui,
+        start_ui,
+        subday_ui,
+        subhour_ui,
+        voltage_ui,
+    )
 
 
 @app.cell
 def _(
     addday_ui,
     addhour_ui,
+    browser_ui,
     end_ui,
     get_hour,
     mo,
@@ -53,35 +115,39 @@ def _(
     subhour_ui,
     timestamp_ui,
     timestamps,
+    voltage_ui,
 ):
     # Show UI inputs
-    mo.hstack([mo.md(str(timestamps[get_hour()])),timestamp_ui,start_ui,subday_ui,subhour_ui,addhour_ui,addday_ui,end_ui],justify='start')
+
+    mo.hstack([voltage_ui,mo.md(str(f"at {timestamps[get_hour()]}")),timestamp_ui,start_ui,subday_ui,subhour_ui,addhour_ui,addday_ui,end_ui,browser_ui],justify='start')
     return
 
 
 @app.cell
 def _(
+    geocodes,
     geojson,
     get_hour,
     interp,
-    loads,
     np,
     plt,
-    timestamp_ui,
+    points,
     timestamps,
     usmap,
+    voltage_ui,
 ):
     # Generate map
-    #
-    _loads = loads.loc[timestamps[timestamp_ui.value]].set_index("geocode")
-    _points = np.stack([_loads.longitude.tolist(), _loads.latitude.tolist()], -1)
-    _values = np.array(_loads["voltage[deg]"].tolist())
-    _x0, _x1 = int(_loads.longitude.min()-1), int(_loads.longitude.max())
-    _y0, _y1 = int(_loads.latitude.min()), int(_loads.latitude.max() + 1)
+
+    _values = np.array(geocodes[voltage_ui.value].tolist())
+    _x0, _x1 = int(geocodes.longitude.min()-1), int(geocodes.longitude.max())
+    _y0, _y1 = int(geocodes.latitude.min()), int(geocodes.latitude.max() + 1)
     _x, _y = np.meshgrid(np.linspace(_x0,_x1,int((_x1-_x0)/0.02)),
                          np.linspace(_y0,_y1,int((_y1-_y0)/0.02)),
                          indexing='xy')
-    _z = interp.griddata(_points, _values, (_x, _y), method="cubic")
+    _z = interp.griddata(points, _values, (_x, _y), method="cubic")
+
+    # draw map
+    plt.figure(figsize=(10,10))
     plt.imshow(_z, extent=[_x0, _x1, _y1, _y0])
 
     # draw states
@@ -90,34 +156,57 @@ def _(
         plt.plot(*_lines,"k")
 
     # draw geocoded points
-    _geocodes = _loads.index.unique()
-    plt.plot(_loads.loc[_geocodes].longitude.tolist(),_loads.loc[_geocodes].latitude.tolist(),'.k')
+    _index = geocodes.index.unique()
+    plt.plot(geocodes.loc[_index].longitude.tolist(),geocodes.loc[_index].latitude.tolist(),'.k')
+
+    # draw empty selection
+    highlight, = plt.plot([],[],'oy')
 
     # finalize map image
     plt.grid()
     plt.xlim([_x0,_x1])
     plt.ylim([_y0,_y1])
     plt.title(f"Voltage angle at {timestamps[get_hour()]}")
+    image = plt.gca()
+    return highlight, image
+
+
+@app.cell
+def _(data_ui, highlight, plt):
+    # selected = np.array(data_ui.value[["longitude","latitude"]].values).transpose().tolist()
+    highlight.set_xdata(data_ui.value["longitude"].values.tolist())
+    highlight.set_ydata(data_ui.value["latitude"].values.tolist())
+    plt.gcf().canvas.draw()
+    plt.gcf().canvas.flush_events()
     return
 
 
 @app.cell
-def _(loads):
-    # Browse data table
-    loads
+def _(image):
+    image
     return
+
+
+@app.cell
+def _(browser_ui, geocodes, get_location, mo, set_location):
+    # Browse data table
+    data_ui = mo.ui.table(geocodes,initial_selection=get_location(),on_change=set_location) if browser_ui.value else None
+    data_ui
+    return (data_ui,)
 
 
 @app.cell
 def _():
     # Load modules
+
     import marimo as mo
     import numpy as np
     import pandas as pd
     import scipy.interpolate as interp
+    import scipy.spatial as spatial
     import matplotlib.pyplot as plt
     import geojson
-    return geojson, interp, mo, np, pd, plt
+    return geojson, interp, mo, np, pd, plt, spatial
 
 
 if __name__ == "__main__":

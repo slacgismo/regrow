a110635198eda331be4b83c319baca5c3b026d93
David P. Chassin <david.chassin@me.com>
2025-01-31 19:21:07 -0800

    Added data to powerplant recording

--- model/recorders.py
+++ model/recorders.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import datetime as dt
+import math
 
 import tape
 
@@ -12,14 +13,29 @@ plant_recorder = None
 def voltage_pu(data):
     return f"{abs(data['V']) / data['Vn']:.3f}"
 
+def voltage_deg(data):
+    V = data['V']
+    return f"{math.atan2(V.imag,V.real)*180/math.pi:.3f}"
+
+def status(x):
+    return ["OFFLINE","ONLINE","CURTAILED"][x]
+
+def generator(x):
+    keywords = dict(CC=1024, PV=512, CT=256, ES=128, WT=64, FW=32, IC=16, AT=8, ST=4, HT=2, UNKNOWN=1)
+    return "|".join([y for y,z in keywords.items() if x&z])
+
+def fuel(x):
+    keywords = dict(NG=32768, COAL=16384, WATER=8192, NUC=4096, GAS=2048, OTHER=1024, WOOD=512, UNKNOWN=256, OIL=128, BIO=64, WASTE=32, COKE=16, GEO=8, SUN=4, WIND=2, ELEC=1)
+    return "|".join([y for y,z in keywords.items() if x&z])
+
 def on_init(t0):
 
     global loads_recorder
-    loads_recorder = tape.GeoRecorder(gridlabd,
+    loads_recorder = tape.GeoRecorder(gldcore,
         csvname="loads.csv",
         objects=r"^wecc240_psse_L_[0-9]+",
         properties={
-            "status" : lambda x:["OFFLINE","ONLINE","CURTAILED"][x],
+            "status" : status,
             "S" : lambda x: f"{abs(x):.1f}",
             },
         alias={
@@ -27,20 +43,27 @@ def on_init(t0):
             },
         virtual={
             "voltage[pu]" : [voltage_pu,["V","Vn"]],
+            "voltage[deg]" : [voltage_deg,["V"]],
         },
         include_latlon=True,
         )
 
     global powerplants_recorder
-    powerplants_recorder = tape.GeoRecorder(gridlabd,
+    powerplants_recorder = tape.GeoRecorder(gldcore,
         csvname="powerplants.csv",
         objects={"class":"powerplant"},
         properties={
-            "status" : lambda x:["OFFLINE","ONLINE","CURTAILED"][x],
+            "status" : status,
+            "generator" : generator,
+            "fuel" : fuel,
             "S" : lambda x: f"{abs(x):.1f}",
+            "operating_capacity": None,
+            "total_cost": None,
             },
         alias={
             "S": "power[MVA]",
+            "operating_capacity": "capacity[MW]",
+            "total_cost": "cost[$]"
             },
         include_latlon=True,            
         )

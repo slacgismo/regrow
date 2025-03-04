b50a3b4e86f892c3eb59016480207626727b3e84
David P. Chassin <david.chassin@me.com>
2025-03-04 06:07:23 -0800

    Update powerplants_aggregation.py

--- model/powerplants_aggregation.py
+++ model/powerplants_aggregation.py
@@ -20,28 +20,49 @@ Output fields are as follows:
 * `units`: the number of units identified of this type at this bus
 
 """
+import os
 import sys
 import json
 sys.path.append("../data")
 import utils
-
-data = json.load(open("wecc240.json","r"))
-assert(data["application"]=="gridlabd")
-objects = data["objects"]
-with open("powerplants_split.csv","w") as fh:
-    print("name,bus,gen,cap,cf,units",file=fh)
-    for name,properties in [(x,y) for x,y in objects.items() if y["class"] == "powerplant"]:
-        bus = properties["parent"]
-        gen = properties["generator"]
-        cap = float(properties["operating_capacity"].split()[0])
-        pmax = float(objects[bus]["Pmax"].split()[0])
-        node = objects[bus]["parent"]
-        lat = float(objects[node]["latitude"])
-        lon = float(objects[node]["longitude"])
-        geo = utils.geohash(lat,lon)
-        for item in gen.split("|"):
-            print(name,geo,item,cap,cap/pmax if pmax>0 else float('nan'),1,sep=",",file=fh)
-
 import pandas as pd
-data = pd.read_csv("powerplants_split.csv",index_col=["bus","gen"],usecols=["bus","gen","cap","cf","units"])
-data.groupby(["bus","gen"]).sum().round(3).to_csv("powerplants_aggregated.csv")
+
+if not os.path.exists("powerplants_aggregated.csv"):
+
+    data = json.load(open("wecc240.json","r"))
+    assert(data["application"]=="gridlabd")
+    objects = data["objects"]
+    with open("powerplants_split.csv","w") as fh:
+        print("name,bus,gen,cap,cf,units",file=fh)
+        for name,properties in [(x,y) for x,y in objects.items() if y["class"] == "powerplant"]:
+            try:
+                bus = properties["parent"].replace("_N_","_G_")
+                gen = properties["generator"]
+                cap = float(properties["operating_capacity"].split()[0])
+                pmax = float(objects[bus]["Pmax"].split()[0])
+                node = objects[bus]["parent"]
+                lat = float(objects[node]["latitude"])
+                lon = float(objects[node]["longitude"])
+                geo = utils.geohash(lat,lon)
+                for item in gen.split("|"):
+                    print(name,geo,item,cap,cap/pmax if pmax>0 else float('nan'),1,sep=",",file=fh)
+            except:
+                e_type,e_value,e_trace = sys.exc_info()
+                print(f"ERROR [{name}]: {e_type.__name__} {e_value}",file=sys.stderr)
+
+    data = pd.read_csv("powerplants_split.csv",index_col=["bus","gen"],usecols=["bus","gen","cap","cf","units"])
+    data.groupby(["bus","gen"]).sum().round(3).to_csv("powerplants_aggregated.csv")
+
+    print(data.groupby(["bus"]).sum(round(3)))
+
+
+# check geocodes
+data = pd.read_csv("powerplants_aggregated.csv")
+weather = pd.read_csv("../data/geodata/temperature.csv")
+buslist = data.bus.unique()
+n = 0
+for bus in data.bus.unique():
+    if bus not in weather.columns:
+        n+=1
+        print(f"WARNING: bus {bus} not found in weather data")
+print(f"{n} of {len(buslist)} busses not found")

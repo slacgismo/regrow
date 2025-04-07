4fe7dae3ec095983c9dacd34eb7660113960fe87
David P. Chassin <david.chassin@me.com>
2025-04-07 13:52:56 -0700

    Quick sim

--- model/quick_sim.py
+++ model/quick_sim.py
@@ -1,6 +1,30 @@
-from arras import gld_pypower
+import gld_pypower
+
+print("WECC 240 Model")
+
 model = gld_pypower.Model("wecc240.json")
-before = model.optimal_powerflow()["curtailment"]
-print(f"Curtailment before optimal sizing: {before.sum():.1f} MW")
+
+gen0 = abs(model.generation().sum())
+cap0 = model.capacitors().sum()
+cur0 = model.optimal_powerflow()["curtailment"].sum()
+
 model.optimal_sizing(margin=0.2,update_model=True)
-print(f"Curtailment after optimal sizing: {after.sum():.1f} MW")
+
+gen1 = abs(model.generation().sum())
+cap1 = model.capacitors().sum()
+cur1 = model.optimal_powerflow()["curtailment"].sum()
+
+print("\nBefore sizing...")
+print(f"Generators: {abs(gen0):.1f} MVA")
+print(f"Capacitors: {cap0:.1f} MVAr")
+print(f"Curtailment: {cur0:.1f} MW")
+
+print("\nSizing impact...")
+print(f"Generators: {gen1-gen0:.1f} MVA")
+print(f"Capacitors: {cap1-cap0:.1f} MVAr")
+print(f"Curtailment: {cur1-cur0:.1f} MW")
+
+print("\nAfter sizing...")
+print(f"Generators: {abs(gen1):.1f} MVA")
+print(f"Capacitors: {cap1:.1f} MVAr")
+print(f"Curtailment: {cur1:.1f} MW")

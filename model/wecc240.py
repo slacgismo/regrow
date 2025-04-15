4add346faf5f8c45706480dfeb79cc6757040dd2
David P. Chassin <david.chassin@me.com>
2025-04-15 07:04:10 -0700

    Fix rename issue

--- model/wecc240.py
+++ model/wecc240.py
@@ -2,7 +2,7 @@ import recorders
 import optimizations
 
 def on_init(t0):
-    return recorders.on_init(t0) and optimal_sizing.on_init(t0)
+    return recorders.on_init(t0) and optimizations.on_init(t0)
 
 def on_commit(t0):
     return recorders.on_commit(t0)

c26accd6ba02bc8e9f0f1ed0c02658ef483f261f
David P. Chassin <david.chassin@me.com>
2025-10-14 09:50:30 -0700

    Update validate.py

--- generation_modeling/validate.py
+++ generation_modeling/validate.py
@@ -2,15 +2,16 @@
 
 from pypower.runopf import runopf
 from wecc240 import wecc240
-import numpy as np
 import pandas as pd
+from gendata import model
 
 # load the WECC model
 data = wecc240()
 
 # load gen data and gen cost files
-gen = pd.read_csv("gen.csv").to_numpy()
-gencost = pd.read_csv("gencost.csv").to_numpy()
+gendata = model("generation_data.csv")
+gen = gendata["gen"]
+gencost = gendata["gencost"]
 
 # convert busname to busid
 busmap = pd.read_csv("bus_data.csv",index_col=['busname'])

89ac554fc397c0906958541fb150fe43e1281ac2
David P. Chassin <david.chassin@me.com>
2025-10-15 08:38:23 -0700

    Update validate.py

--- generation_modeling/validate.py
+++ generation_modeling/validate.py
@@ -2,24 +2,88 @@
 
 from pypower.runopf import runopf
 from wecc240 import wecc240
+import numpy as np
 import pandas as pd
-from gendata import model
+import json
 
-# load the WECC model
-data = wecc240()
+glm = json.load(open("../model/wecc240.json","r"))["objects"]
+
+busids = pd.read_csv("bus_data.csv",index_col=[0],dtype=int)
+def busid(n:int|float|str):
+    return busids.loc[int(n)].busID
+
+pd.options.display.width = None
+pd.options.display.max_columns = None
+np.set_printoptions(formatter={'float_kind':"{:12g}".format},linewidth=10000,edgeitems=1000)
 
 # load gen data and gen cost files
-gendata = model("generation_data.csv")
-gen = gendata["gen"]
-gencost = gendata["gencost"]
+gen = pd.read_csv("gen.csv")
+gencost = pd.read_csv("gencost.csv")
 
-# convert busname to busid
-busmap = pd.read_csv("bus_data.csv",index_col=['busname'])
-gen[:,0] = [float(busmap.loc[x].busID) for x in gen[:,0]]
+# load the line data
+lines = pd.read_csv("line_data.csv",
+    skiprows=1,
+    names=['FBUS','TBUS','BR_X','RATE_A','BR_STATUS'],
+    )
 
-# replace default gen/gencost arrays in model
-data["gen"] = gen
-data["gencost"] = gencost
+# construct bus and branch data
+genlist = set([int(x) for x in gen.GEN_BUS])
+bus = {}
+branch = []
+ref = None
+for n,line in lines.iterrows():
+    if line.FBUS in bus and line.TBUS in bus:
+        if line.RATE_A == 99999:
+            print(f"WARNING [line_data.csv]: line {n} from {line.FBUS:.0f} to {line.TBUS:.0f} already provided with a usable rating")
+        else:
+            print(f"WARNING [line_data.csv]: line {n} is a duplicate with different values")
+        continue
+    elif line.RATE_A == 99999:
+        print(f"WARNING [line_data.csv]: line {n} rating 99999 is set to zero for pypower default")
+        line.RATE_A = 0.0
+    for BUS_I in [line.FBUS,line.TBUS]:
+        if not BUS_I in bus:
+            obj = glm[f"wecc240_psse_N_{BUS_I:.0f}"]
+            BUS_TYPE = 2 if int(BUS_I) in genlist else 1
+            if BUS_TYPE == 2 and ref is None:
+                BUS_TYPE = 3
+                ref = BUS_I
+            PD = float(obj["Pd"].split()[0])
+            QD = float(obj["Qd"].split()[0])
+            GS = float(obj["Gs"].split()[0])
+            BS = float(obj["Bs"].split()[0])
+            BUS_AREA = float(obj["area"])
+            VM = float(obj["Vm"].split()[0])
+            VA = float(obj["Va"].split()[0])
+            BASE_KV = float(obj["baseKV"].split()[0])
+            ZONE = float(obj["zone"])
+            VMAX = float(obj["Vmax"].split()[0])
+            VMIN = float(obj["Vmin"].split()[0])
+            bus[BUS_I] = [busid(BUS_I),BUS_TYPE,PD,QD,GS,BS,BUS_AREA,VM,VA,BASE_KV,ZONE,VMAX,VMIN]
+    branch.append([busid(line.FBUS),busid(line.TBUS),0,line.BR_X,0,line.RATE_A,0,0,0,0,line.BR_STATUS,-360,+360])
+bus = np.array(sorted(list(bus.values())))
+branch = np.array(sorted(branch))
+
+gen.GEN_BUS = [busid(x) for x in gen.GEN_BUS]
+
+model = {
+    "version" : 2,
+    "baseMVA" : 100.0,
+    "bus" : bus,
+    "branch" : branch,
+    "gen" : gen.to_numpy(),
+    "gencost" : gencost.to_numpy(),
+}
+
+# # Output pypower case data
+# print("from numpy import array")
+# print("{")
+# for x,y in model.items():
+#     if isinstance(y,(int,float)):
+#         print(f"    {x}: {y},")
+#     else:
+#         print(f"    {x}: array(\n      [{',\n       '.join([str(z) for z in y])}\n      ]),")
+# print("}")
 
 # run full AC OPF
-runopf(data)
+runopf(model)

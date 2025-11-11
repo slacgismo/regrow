87e3a0959548cdcafd09d4f3dd124a74e97b3f72
David P. Chassin <david.chassin@me.com>
2025-11-10 19:07:47 -0800

    Update review.py

--- data/EIA/review.py
+++ data/EIA/review.py
@@ -16,7 +16,7 @@ def _(mo):
         r"""
     # EIA Form 861 Data
 
-    Annual electric power industry reports detailed data files.
+    Annual electric power industry reports detailed data in Form 861, as shown in Table 1.
 
     Source: https://www.eia.gov/electricity/data/eia861/
     """
@@ -54,8 +54,17 @@ def _(fix_2019_ui, fix_WATRA_ui, mo, pd):
     if fix_WATRA_ui.value:
         for _x in data.index.get_level_values(1).unique()[1:]:
             data.loc["WA",_x].TRA = float('NAN')#/= 10
-    mo.ui.table(data,page_size=len(data.index.get_level_values(1).unique()),selection=None)
+    mo.vstack([
+        mo.md("**Table 1: Form 861 Annual Energy Use (GWh/y)**"),
+        mo.ui.table(data,page_size=len(data.index.get_level_values(1).unique()),selection=None)
+        ])
     return (data,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""The load growth is computed by divide the energy use for each year by the energy use in 2018, as shown in Figure 1.""")
+    return
 
 
 @app.cell
@@ -67,20 +76,29 @@ def _(data, mo):
 
 
 @app.cell
-def _(data, state_ui):
-    # print(data)
-    growth_by_year = (
-        data.loc[state_ui.value,:]
-        / data.loc[state_ui.value,"2018"]
-    )
-    # print(growth)
-    growth_by_year.plot(
-        title=f"EIA Form 861 Data for {state_ui.value}",
+def _(data, mo, state_ui):
+    growth_by_year = data.loc[state_ui.value, :] / data.loc[state_ui.value, "2018"]
+    _result = (growth_by_year * 100 - 100).plot(
+        figsize=(15, 6),
         grid=True,
         xlabel="Year",
-        ylabel="Load growth (%/y)",
+        ylabel="Load growth (% w.r.t 2018)",
         legend="outside",
     )
+    mo.vstack(
+        [
+            _result,
+            mo.md(
+                f"**Figure 1: {state_ui.value} annual load growth relative for 2018.**"
+            ),
+        ]
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""The state-level load growth by end-use sector is shown in Figure 2 for the following year.""")
     return
 
 
@@ -93,24 +111,47 @@ def _(data, mo):
 
 
 @app.cell
-def _(data, year_ui):
+def _(data, mo, year_ui):
     growth_by_state = (
         data.reset_index().set_index(["year", "state"]).loc[year_ui.value, :]
         / data.reset_index().set_index(["year", "state"]).loc["2018", :]
     )
-    growth_by_state.plot(figsize=(15,7),kind="bar", grid=True,title=f"Load growth from 2018 to {year_ui.value}")
+    _result = (growth_by_state * 100 - 100).plot(
+        figsize=(15, 7),
+        kind="bar",
+        grid=True,
+        xlabel="State",
+        ylabel=f"Load growth (% w.r.t 2018)",
+    )
+    mo.vstack(
+        [
+            _result,
+            mo.md(
+                "**Figure 2: load growth by state for {year_ui.value} relative to 2018.**"
+            ),
+        ]
+    )
     return (growth_by_state,)
 
 
 @app.cell
 def _(mo):
-    mo.md(r"""## Results""")
+    mo.md(
+        r"""
+    ## Results
+
+    The fractional load growth w.r.t 2018 are shown in Table 2.
+    """
+    )
     return
 
 
 @app.cell
-def _(growth_by_state):
-    growth_by_state.round(3)
+def _(growth_by_state, mo):
+    mo.vstack([
+        mo.md("**Table 2: Fractional load growth by state for each end-use sector w.r.t. to 2018.**"),
+        growth_by_state.round(3),
+        ])
     return
 
 
@@ -120,7 +161,11 @@ def _(mo):
         r"""
     # EIA Form 930 Data
 
-    Utility demand data by balancing authority and utility. Source: https://s3.us-west-2.amazonaws.com/pudl.catalyst.coop/nightly/core_eia930__hourly_subregion_demand.parquet. Documentation: https://catalystcoop-pudl.readthedocs.io/en/stable/data_sources/eia930.html
+    Utility demand data by balancing authority and utility for IOUs is available from EIA Form 930, as shown in Figure 3.
+
+    Source: https://s3.us-west-2.amazonaws.com/pudl.catalyst.coop/nightly/core_eia930__hourly_subregion_demand.parquet. 
+
+    Documentation: https://catalystcoop-pudl.readthedocs.io/en/stable/data_sources/eia930.html
     """
     )
     return
@@ -149,30 +194,56 @@ def _(demand, mo, region_ui):
 
 
 @app.cell
-def _(demand, region_ui, utility_ui):
-    (demand.loc[region_ui.value,utility_ui.value]/1e3).plot(grid=True,figsize=(15,7),legend=None)
+def _(demand, mo, region_ui, utility_ui):
+    mo.vstack([
+        (demand.loc[region_ui.value,utility_ui.value]/1e3).plot(grid=True,figsize=(15,7),legend=None),
+        mo.md(f"**Figure 3: EIA Form 930 data for {utility_ui.value}.**")
+        ])
     return
 
 
 @app.cell
-def _(demand, dt, np, plt):
-    _data = demand.loc[:,:,dt.datetime(2019,1,1,0,0,0):dt.datetime(2024,12,31,23,59,59)].groupby("datetime_utc").sum().sort_index()/1000
-    _data.drop(_data.loc[_data["demand_reported_mwh"]<1].index,inplace=True,axis=0)
-    # _data.plot()
-    _index = (_data.index-dt.datetime(2018,1,1,0,0,0)).total_seconds()/86400/365.2425
-    _x = [_index[0],_index[-1]]
-    _fit0 = np.polyfit(_index,_data.demand_reported_mwh,0)
-    _fit1 = np.polyfit(_index,_data.demand_reported_mwh,1)
-    # print([_data.index[0],_data.index[-1]],_fit1,np.polyval(_fit1,_x))
-    plt.figure(figsize=(15,7))
-    plt.plot(_data,".",markersize=0.2,label="Total IOU Demand (GW)")
-    plt.plot([_data.index[0],_data.index[-1]],np.polyval(_fit0,_x),label=f"Mean ({_fit0[0]:.1f} GW)")
-    plt.plot([_data.index[0],_data.index[-1]],np.polyval(_fit1,_x),label=f"Trend ({_fit1[0]/_fit1[1]*100:.1f}%/y)")
-    plt.title("EIA Form 930 Data for California IOUs")
-    plt.xlabel("Demand (GW)")
-    plt.legend()
-    plt.grid()
-    plt.gca()
+def _(data, demand, dt):
+    form930 = (
+        demand.loc[
+            :,
+            :,
+            dt.datetime(2019, 1, 1, 0, 0, 0) : dt.datetime(
+                2022, 12, 31, 23, 59, 59
+            ),
+        ]
+        .groupby(["datetime_utc"])
+        .sum()
+    )
+    form930["year"] = form930.index.year
+    form930 = form930.groupby("year").sum() / 8766
+    iou_fraction = form930.demand_reported_mwh.mean()/1000/(data.loc["CA"].TOT/8766*1000).mean()*100
+    return form930, iou_fraction
+
+
+@app.cell
+def _(iou_fraction, mo):
+    mo.md(rf"""The EIA Form 930 data provides the demand for IOUs in California, which represent {iou_fraction:.1f}% of the total energy demand in California reported by Form 861 data. The different growth rates for Form 861 data for the entire state and Form 960 data are shown in Figure 4.""")
+    return
+
+
+@app.cell
+def _(data, form930, mo, np):
+    _fit = np.polyfit(form930.index - 2018, form930.demand_reported_mwh, 1)[1]
+    _data = form930.copy()
+    _data.loc[2018] = _fit
+    _data.sort_index(inplace=True)
+    _data["Form 930"] = _data.demand_reported_mwh / _fit
+    _data["Form 861"] = (data.loc["CA", :] / data.loc["CA", "2018"]).TOT.tolist()
+    mo.vstack([
+        (_data[["Form 861", "Form 930"]] * 100 - 100).sort_index().plot(
+            figsize=(15,6),
+            grid=True,
+            ylabel="Load growth (% w.r.t 2018)",
+            xticks=range(2018, 2023),
+        ),
+        mo.md("**Figure 4: California IOU and state-wide load growth as reported by EIA.**")
+        ])
     return
 
 
@@ -184,7 +255,7 @@ def _():
     import numpy as np
     import matplotlib.pyplot as plt
     import datetime as dt
-    return dt, mo, np, pd, plt
+    return dt, mo, np, pd
 
 
 if __name__ == "__main__":
